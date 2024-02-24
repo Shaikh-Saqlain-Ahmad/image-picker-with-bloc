9540d5b65f69a0be1a7914051201652b3e382bf5
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-02-24 19:31:17 +0500

    image picker bloc completed

--- lib/bloc/image_picker_bloc.dart
+++ lib/bloc/image_picker_bloc.dart
@@ -1,1 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_picker_with_bloc/bloc/image_picker_event.dart';
+import 'package:image_picker_with_bloc/bloc/image_picker_state.dart';
+import 'package:image_picker_with_bloc/utils/image-picker-utils.dart';
 
+class ImagePickerBloc extends Bloc<ImagePickerEvent, ImagePickerState> {
+  final ImagePickerUtils imagePickerUtils;
+  ImagePickerBloc(this.imagePickerUtils) : super(const ImagePickerState()) {
+    on<CameraCaptureEvent>(cameraCapture);
+    on<CameraCaptureEvent>(galleryCapture);
+  }
+  void cameraCapture(
+      CameraCaptureEvent event, Emitter<ImagePickerState> states) async {
+    XFile? file = await imagePickerUtils.cameraCapture();
+    emit(state.copyWith(file: file));
+  }
+
+  void galleryCapture(
+      CameraCaptureEvent event, Emitter<ImagePickerState> states) async {
+    XFile? file = await imagePickerUtils.pickImageFromGallery();
+    emit(state.copyWith(file: file));
+  }
+}
